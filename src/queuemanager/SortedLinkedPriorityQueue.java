6d70f16f9624738b6def1639aa5051ab3928b21e
vaijuGore <sanjivaniborgaonkar@gmail.com>
2018-03-17 22:13:59 +0000

    not correct at all

--- src/queuemanager/SortedLinkedPriorityQueue.java
+++ src/queuemanager/SortedLinkedPriorityQueue.java
@@ -8,7 +8,72 @@ package queuemanager;
 /**
  *
  * @author sudhi
+ * @param <T>
  */
-public class SortedLinkedPriorityQueue {
+
+
+/**
+ *
+ * @author sudhi
+ */
+public class SortedLinkedPriorityQueue<T> extends  ListNode implements PriorityQueue<T>  {
+    
+     private ListNode<T> top;
+
+    public SortedLinkedPriorityQueue(ListNode<T> top, ListNode nextNode, PriorityItem item) {
+        super(nextNode, item);
+        this.top = top;
+    }
+
+    
+ @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+    
+    @Override
+    public T head() {
+        if (isEmpty()) {
+            throw new StackUnderflowException();
+        }
+        return top.getItem();
+    }
+    
+    @Override
+    public void add(PriorityItem<T> item) {
+        if (isEmpty()) {
+            throw new StackUnderflowException();
+        }
+      
+    }
+    
+    @Override
+    public void remove(PriorityItem<T> item) {
+        for (int i = 0; i < 10; i++) {
+            while (it.hasNext()) {
+                Object next = it.next();
+                top = new ListNode<>(item);
+            }
+        }
+    }
+    
+    @Override
+    public String toString() {
+        String result = "LinkedStack: size = " + size();
+        result += ", contents = [";
+        for (ListNode<T> node = top; node != null; node = node.getNextNode()) {
+            if (node != top) {
+                result += ", ";
+            }
+            result += node.getItem();
+        }
+        result += "], isEmpty() = " + isEmpty();
+        if (!isEmpty()) {
+            result += ", top() = " + top();
+        }
+        return result;
+    }
+    
     
 }
+
