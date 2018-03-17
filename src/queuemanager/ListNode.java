669ebebf5d8560ab7e95037948e612a21e8653b0
vaijuGore <sanjivaniborgaonkar@gmail.com>
2018-03-17 13:30:16 +0000

    class listnode

---
+++ src/queuemanager/ListNode.java
@@ -0,0 +1,60 @@
+package queuemanager;
+
+public class ListNode {
+      
+        private ListNode nextNode;
+       // private PriorityQueue<Person> q;
+          private int data;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    ListNode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public ListNode getNextNode() {
+        return nextNode;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setNextNode(ListNode nextNode) {
+        this.nextNode = nextNode;
+    }
+
+   
+}
+          
+       
+
+  
+       
+
+   
+
+         
+       
+
+    
+      
+      
+
+      
+       
+
+    
+     
+
+            
+                    
+
+
+    
